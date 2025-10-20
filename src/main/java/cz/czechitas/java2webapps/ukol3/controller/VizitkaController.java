824c4c382e30de6fe4ced89ef2ac56f455c66a0a
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-10-20 11:26:47 +0200

    feat: append new vizitka to the list

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,9 +1,11 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import cz.czechitas.java2webapps.ukol3.service.VizitkaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -29,5 +31,16 @@ public class VizitkaController {
         ModelAndView result = new ModelAndView("/detail");
         result.addObject("vizitka", service.getById(id));
         return result;
+    }
+
+    @GetMapping("/nova")
+    public ModelAndView nova() {
+        return new ModelAndView("nova");
+    }
+
+    @PostMapping("/nova")
+    public String append(Vizitka vizitka) {
+        service.append(vizitka);
+        return "redirect:/";
     }
 }
--- src/main/java/cz/czechitas/java2webapps/ukol3/service/VizitkaService.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/service/VizitkaService.java
@@ -26,4 +26,8 @@ public class VizitkaService {
     public Vizitka getById(int id) {
         return seznamVizitek.get(id);
     }
+
+    public void append (Vizitka vizitka) { seznamVizitek.add(vizitka); }
+
+    public void deleteById(int id) { seznamVizitek.remove(id); }
 }
